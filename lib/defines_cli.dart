e53d5a06a7440ad8f98989f1ddd205bcd2585c20
Alba <alba__22@outlook.com>
2023-04-29 08:55:49 -0300

    create base CLI project

---
+++ lib/defines_cli.dart
@@ -0,0 +1,10 @@
+/// defines_cli, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate defines_cli
+/// dart pub global activate defines_cli
+///
+/// # see usage
+/// defines_cli --help
+/// ```
+library defines_cli;
